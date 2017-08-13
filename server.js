a359f570f78371cace2552e02d4c7fe80172bb64
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2017-08-13 23:04:19 +0100

    Tweaks and cleanup. Started experimenting with CryptoJS.AES

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*
-    ____                  ________          __ 
+    ____                  ________          __
    / __ \___  ____  _____/ ____/ /_  ____  / /_
   / /_/ / _ \/ __ \/ ___/ /   / __ \/ __ \/ __/
- / ____/ ___/ /_/ / /  / /___/ / / / /_/ / /_  
-/_/    \___/\__,_/_/   \____/_/ /_/\__,_/\__/  
+ / ____/ ___/ /_/ / /  / /___/ / / / /_/ / /_
+/_/    \___/\__,_/_/   \____/_/ /_/\__,_/\__/
 
 Created by Robbie Jakob-Whitworth during the summer of 2017
 
@@ -35,14 +35,14 @@ app.use(express.static('public'));
 
 
 // Set view engine to ejs (templating application) https://scotch.io/tutorials/use-ejs-to-template-your-node-application
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 
 
 // root
 app.get("/", function(req, res) {
 	res.send("<script>window.location.href='/chat';</script>");
-	console.log("Got GET request for / from " + ipV6toV4(req.ip) + ", sent '<script>window.location.href='/chat';</script>'");
-	
+	console.log("Got GET request for / from " + ipV6toV4(req.ip) + ", sent '<script>window.location.href=\"chat\";</script>'");
+
 });
 
 
@@ -78,13 +78,13 @@ app.get("/gen_mtoken", function(req, res) {
 // /chat
 app.get("/chat", function(req, res) {
 	console.log("Got GET request for /chat from " + ipV6toV4(req.ip));
-	
+
 	/* Do chat setup */
 	var visitor = ipV6toV4(req.ip);
 	var h_or_v = isHostOrVisitor(ipV6toV4(req.ip));
-	
+
 	var cs;
-	
+
 	if(h_or_v == "host") {
 		var unpairedSessions = getUnpairedChatSessions();
 		if(unpairedSessions.length == 1) {
@@ -100,11 +100,11 @@ app.get("/chat", function(req, res) {
 		cs = new ChatSession(visitor, sToken, hToken, vToken);
 		sessions[sessions.length] = cs;
 	}
-	
+
 	var esjData = {
 		"wcToken": ""
 	}
-	
+
 	if(h_or_v == "host") {
 		esjData.wcToken = cs.hToken;
 		cs.hostHasJoined = true;
@@ -112,11 +112,11 @@ app.get("/chat", function(req, res) {
 		esjData.wcToken = cs.vToken;
 		cs.visitorHasJoined = true;
 	}
-	
+
 	res.render("ejs_pages/chat_client_interface", esjData);
 	console.log("Set up new ChatSession:", cs);
-	
-	
+
+
 });
 
 
@@ -124,7 +124,7 @@ app.get("/chat", function(req, res) {
 app.post("/chat_send", urlencodedParser, function(req, res) {
 	// Do something with a sent message
 	console.log(ipV6toV4(req.ip) + " sent jqXHR:", JSON.stringify(req.body));
-	
+
 	var cs = getChatSessionFromWCToken(req.body.wcToken);
 	if(cs == null) {
 		asyncChatSendResponse({"success":false, "mToken": req.body.mToken, "errString": "No existing ChatSession with wcToken " + req.body.wcToken + " could be found"}, req, res);
@@ -134,11 +134,11 @@ app.post("/chat_send", urlencodedParser, function(req, res) {
 	var content;
 	var timestamp;
 	var h_or_v;
-	
+
 	// Do validation checks
 	// Check mToken
 	if(req.body.mToken == undefined) {
-		asyncChatSendResponse({"success":false, "mToken": null, "errString": "No mToken was specified"}, req, res);	
+		asyncChatSendResponse({"success":false, "mToken": null, "errString": "No mToken was specified"}, req, res);
 		return;
 	} else {
 		mToken = req.body.mToken;
@@ -171,9 +171,9 @@ app.post("/chat_send", urlencodedParser, function(req, res) {
 	} else {
 		timestamp = req.body.timestamp;
 	}
-	
+
 	var msg = new Message(sender, content, mToken, timestamp);
-	
+
 	if(h_or_v == "host") {
 		cs.pendingVMessages[cs.pendingVMessages.length] = msg;
 		console.log("Appended message " + mToken + " to pendingVMessages. pvm is now " + cs.pendingVMessages.length + " long.");
@@ -183,7 +183,7 @@ app.post("/chat_send", urlencodedParser, function(req, res) {
 	} else {
 		// Something went wrong
 	}
-	
+
 	// Assuming everything went well
 	asyncChatSendResponse({"success": true, "mToken": req.body.mToken}, req, res);
 });
@@ -196,21 +196,21 @@ var asyncChatSendResponse = function(response, req, res) {
 
 // /chat_pull
 app.post("/chat_pull", urlencodedParser, function(req, res) {
-	
+
 	// Check that they actually sent wcToken
 	if(req.body.wcToken == undefined) {
-		asyncChatSendResponse({"success": false, "errString": "No wcToken provided"}, req, res);		
+		asyncChatSendResponse({"success": false, "errString": "No wcToken provided"}, req, res);
 	}
-	
+
 	console.log(ipV6toV4(req.ip) + " sent POST to /chat_pull with wcToken " + req.body.wcToken);
-	
+
 	var wcToken = req.body.wcToken;
 	cs = getChatSessionFromWCToken(wcToken);
-	
+
 	if(cs == null) {
 		asyncChatSendResponse({"success": false, "errString": "No ChatSession was found with which the given wcToken is associated", "wcToken": wcToken}, req, res);
 	}
-	
+
 	// Push the queued messages to the web client
 	var refreshTime = 50; // Time to wait before rechecking the queue for new messages (milliseconds)
 	if(cs.vToken == wcToken) {
@@ -220,14 +220,14 @@ app.post("/chat_pull", urlencodedParser, function(req, res) {
 			timesChecked++;
 			if(timesChecked * refreshTime > req.body.timeout) {
 				clearInterval(loopID);
-				console.log("Cleared interval vToken " + loopID);
+				console.log("Cleared interval vToken");
 			}
 			if(cs.pendingVMessages.length > 0) {
 				console.log("cs.pendingVMessages.length = " + cs.pendingVMessages.length);
 				res.send(JSON.stringify({"messages":cs.pendingVMessages}));
 				console.log("Sent " + cs.pendingVMessages.length + " messages to visitor /chat_pull");
 				clearInterval(loopID);
-				console.log("Cleared interval vToken " + loopID);
+				console.log("Cleared interval vToken");
 				cs.messages = cs.messages.concat(cs.pendingVMessages);
 				cs.pendingVMessages = [];
 			}
@@ -239,29 +239,29 @@ app.post("/chat_pull", urlencodedParser, function(req, res) {
 			timesChecked++;
 			if(timesChecked * refreshTime > req.body.timeout) {
 				clearInterval(loopID);
-				console.log("Cleared interval hToken " + loopID);
+				console.log("Cleared interval hToken");
 			}
 			if(cs.pendingHMessages.length > 0) {
 				console.log("cs.pendingHMessages.length = " + cs.pendingHMessages.length);
 				res.send(JSON.stringify({"messages":cs.pendingHMessages}));
 				console.log("Sent " + cs.pendingHMessages.length + " messages to host /chat_pull");
 				clearInterval(loopID);
-				console.log("Cleared interval hToken " + loopID);
+				console.log("Cleared interval hToken");
 				cs.messages = cs.messages.concat(cs.pendingHMessages);
 				cs.pendingHMessages = [];
 			}
 		}, refreshTime);
 	}
 
-	
+
 });
 
 
 
 var ipV6toV4 = function(ip) {
-	console.log("Called ipV6toV4 with arg " + ip);
+	// console.log("Called ipV6toV4 with arg " + ip);
 	var toReturn = ip === "::1" ? "localhost" : (ip.split(".").length-1 > 0 ? ip.substring(ip.lastIndexOf(":")+1) : ip);
-	console.log("Returned " + toReturn);
+	// console.log("Returned " + toReturn);
 	return toReturn;
 }
 
@@ -303,10 +303,10 @@ var isHostOrVisitor = function(req_ip) {
 	}
 }
 
-/* Returns a ChatSession object which has the given wcToken associated with it 
+/* Returns a ChatSession object which has the given wcToken associated with it
  * Returns null if no ChatSession found which matches the given criteria */
 var getChatSessionFromWCToken = function(wcToken) {
-	if(!usedTokens.includes(wcToken)) { // The token was never used, so it cannot be associated with a ChatSession 
+	if(!usedTokens.includes(wcToken)) { // The token was never used, so it cannot be associated with a ChatSession
 		console.log("wcToken " + wcToken + " was not in usedTokens");
 		console.log(usedTokens);
 		return null;
@@ -337,7 +337,7 @@ var getUnpairedChatSessions = function() {
 /* Create a ChatSession class */
 var ChatSession = function(visitor, sToken, hToken, vToken) {
 	this.visitor = visitor;				// Visitor's IP
-	this.startTime = prettyTimeNow();	// Start time of the 
+	this.startTime = prettyTimeNow();	// Start time of the
 	this.messages = [];					// Messages that have already been sent, these can probably be periodically removed
 	this.pendingHMessages = [];			// Messages sent FROM the visitor that are waiting to be sent TO the host
 	this.pendingVMessages = [];			// Messages sent FROM the host that are waiting to be sent TO the visitor
@@ -362,6 +362,6 @@ var Message = function(sender, content, mToken, timestamp) {
 var server = app.listen(SERVER_PORT, function() {
 	var host = ip.address();
 	var port = server.address().port;
-	
+
 	console.log("App listening at http://%s:%s", host, port);
 });
